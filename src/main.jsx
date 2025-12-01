bfcc03a53c69dcf51e9dd737cb08fcd2f76246da
Shahbaj Siddique <shahbajs593@gmail.com>
2025-12-01 20:48:28 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-// import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./context/userProvider.jsx";
-import { UserLinkProvider } from "./context/userLink/userLinkProvider.jsx";
+import { UserProvider } from "./context/UserProvider.jsx";
+import { UserLinkProvider } from "./context/userLink/UserLinkProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ErrorBoundry from "./components/errorBoundry/ErrorBoundry.jsx";
 
@@ -22,5 +22,3 @@ createRoot(document.getElementById("root")).render(
   </QueryClientProvider>
   </ErrorBoundry>
 );
-//strictmode? StrictMode is a tool for highlighting potential problems in an application. It activates additional checks and warnings for its descendants. It helps with identifying unsafe lifecycles, legacy API usage, and other side effects.
-//useParams hook? The useParams hook in React Router is used to access the parameters of the current route. It allows you to extract dynamic segments from the URL, making it easy to work with route parameters in your components. When a route is defined with parameters (e.g., /user/:id), you can use the useParams hook to retrieve the values of those parameters as an object. This is particularly useful for rendering content based on specific identifiers, such as user IDs or product IDs, that are part of the URL. By using useParams, you can easily access and utilize these parameters within your component logic.
